6074b6ed8918e42e715e16a9fecf64528b1b326f
78080455 <40352546+78080455@users.noreply.github.com>
2018-07-11 00:35:53 +0800

    201807110035001
    ........

--- js/js.js
+++ js/js.js
@@ -1,3 +1,29 @@
+$(window).scroll(function(event) {
+			//规则;
+			var topHeight = 60;
+			var topWidth = 180;
+			var topNavMarginTop = 11;
+
+			//滚动到多少像素时发生
+			var trans_position = 60;
+
+			/* 事件 */
+			var scrollTop = $(window).scrollTop();
+			if(scrollTop > trans_position){
+				$('.top,.topContent,.topLogo,.topBg').css('height', topHeight);
+				$('.topLogo').css('width', topWidth);
+				$('.navMarginTop').css('margin-top', topNavMarginTop);
+
+			} else {
+				//最顶部时候
+				$('.top,.topContent,.topLogo,.topBg').css('height', 90);
+				$('.topLogo').css('width', 270);
+				$('.navMarginTop').css('margin-top', 41);
+
+			}
+		});
+
+
 $(document).ready(function(){
 	$('.number').each(function(){
 		$(this).prop('Counter',0).animate({
