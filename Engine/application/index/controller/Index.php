085bf7c454bcd7862cdc8942205f75407f2837ca
Jahs <jxtory@sina.com>
2018-07-11 01:30:30 +0800

    201807110130
    模块化

--- Engine/application/index/controller/Index.php
+++ Engine/application/index/controller/Index.php
@@ -8,6 +8,9 @@ class Index extends Controller
     public function index()
     {
     	// 渲染首页
+        $indexTitle = "首页";
+        $this->assign("indexTitle", $indexTitle);
     	return $this->fetch("index");
     }
+
 }
--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 ﻿<?php
 	//前端调试时
-	echo "<script>self.location.href='http://localhost/index.html';</script>";
+	// echo "<script>self.location.href='http://localhost/index.html';</script>";
 	// 程序位置
-	define('APP_PATH', __DIR__ . '/Jahs_Vexy/application/');
+	define('APP_PATH', __DIR__ . '/Engine/application/');
 	// 网站根目录
 	define('MySite', __DIR__);
 	// 扩展目录
 	// define('EXTEND_PATH', 'Jahs_VexyPHP');
 	// 引擎文件引入
-	require __DIR__ . './Jahs_Vexy/thinkphp/start.php';
+	require __DIR__ . './Engine/thinkphp/start.php';
 ?>
