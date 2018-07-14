ee265b79fc2375904107e37703a14a16ff411104
Jahs <jxtory@sina.com>
2018-07-14 23:24:56 +0800

    201807142324
    一些说明

--- Engine/application/index/controller/Index.php
+++ Engine/application/index/controller/Index.php
@@ -10,6 +10,16 @@ class Index extends RCKBase
     	// 渲染首页
         $indexTitle = "首页";
         $this->assign("indexTitle", $indexTitle);
+
+        $url_Manager = url("manager/index/index");
+
+        // 开发中首页开放入口
+        if(APD){
+	        $this->assign('port_Manager', "<div style=\"text-align: center;font-size: 16px;\"><a href=\"{$url_Manager}\" target=\"_blank\">后台入口</a> |-<i>仅开发中可见</i></div>");
+        } else {
+        	$this->assign('port_Manager', '');
+        }
+
     	return $this->fetch("index");
     }
 
--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 ﻿<?php
 	//前端调试时
 	// echo "<script>self.location.href='http://localhost/index.html';</script>";
-	// 程序位置
-	define('APP_PATH', __DIR__ . '/Engine/application/');
-	// 网站根目录
-	define('MySite', __DIR__);
+
+	// 常用常量
+	define('APP_PATH', __DIR__ . '/Engine/application/');			//程序位置
+	define('MySite', __DIR__);										//网站根目录
+	define('APD', true);											//开发状态
+
 	// 扩展目录
 	// define('EXTEND_PATH', 'Jahs_VexyPHP');
 	// 引擎文件引入
