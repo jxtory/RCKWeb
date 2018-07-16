6ffc5b93681053ced3d75495f8e59fcdf01dfa90
Jahs <jxtory@sina.com>
2018-07-16 22:21:36 +0800

    201807162221
    前端设计师失踪了， 要不要报警寻找?

--- Engine/application/index/controller/Index.php
+++ Engine/application/index/controller/Index.php
@@ -14,8 +14,11 @@ class Index extends RCKBase
         $url_Manager = url("manager/index/index");
 
         // 开发中首页开放入口
+        $frontcode = "<!-- S乱入代码 -->\n\t";
+        $frontcode .= "<div style=\"text-align: center;font-size: 16px;\"><a href=\"{$url_Manager}\" target=\"_blank\">后台入口</a> |-<i>仅开发中可见</i></div><div style=\"text-align: center;font-size: 16px;\">我们的<i><b>前端</b>设计师</i>一直处于失踪的状态！首页还没出完就无影无踪了</div>";
+        $frontcode .= "\n\t<!-- E乱入代码 -->";
         if(APD){
-	        $this->assign('port_Manager', "<div style=\"text-align: center;font-size: 16px;\"><a href=\"{$url_Manager}\" target=\"_blank\">后台入口</a> |-<i>仅开发中可见</i></div>");
+	        $this->assign('port_Manager', $frontcode);
         } else {
         	$this->assign('port_Manager', '');
         }
