4f28ce548bfe803ecfa738e238ba3e822e3e750f
Jahs <jxtory@sina.com>
2018-07-24 00:08:30 +0800

    201807240008
    轮播图库删除

--- Engine/application/manager/controller/Uploadcenter.php
+++ Engine/application/manager/controller/Uploadcenter.php
@@ -5,6 +5,8 @@ use \think\Controller;
 
 class Uploadcenter extends ManagerBase
 {
+    private $bannerPath = "uploadcenter/banner";
+
     public function index()
     {
     	return $this->fetch("index");
@@ -28,12 +30,33 @@ class Uploadcenter extends ManagerBase
             foreach ($files as $file) {
                 // 文件取名规则 —— function myUploadRule 定义在 Common.php
                 $mUR = 'myUploadRule';
-                $info = $file->rule($mUR)->move('uploadcenter/banner');
+                $info = $file->rule($mUR)->move($this->bannerPath);
             }
 
         }
 
     	return $this->fetch('banner_add');
+    }
+
+    public function uploadcenter_handle()
+    {
+        // 各种提交请求处理
+        if(input("post.types") == "delBanner"){
+            $datas = input();
+            unset($datas['types']);
+            $files = explode(",", $datas['files']);
+
+            foreach ($files as $value) {
+                if(strlen($value) > 0){
+                    if(!unlink($this->bannerPath . DS . $value)){
+                        exception('删除Banner错误');
+                        // throw new Exception("删除Banner错误");
+                    }
+                }
+            }
+
+            return true;
+        }
     }
 
 }
