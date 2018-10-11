9fc658c6e839e822fd9f32caa1e8c3011b4d32ad
Jahs <jxtory@sina.com>
2018-10-12 01:51:30 +0800

    201810120151
    数据库完善

--- Engine/application/cnpse.sql
+++ Engine/application/cnpse.sql
@@ -4,10 +4,46 @@ Source Host: localhost
 Source Database: cnpse
 Target Host: localhost
 Target Database: cnpse
-Date: 2018/9/10 1:22:15
+Date: 2018/10/12 1:50:32
 */
 
 SET FOREIGN_KEY_CHECKS=0;
+-- ----------------------------
+-- Table structure for cnpse_column
+-- ----------------------------
+CREATE TABLE `cnpse_column` (
+  `id` int(11) NOT NULL,
+  `columnname` text NOT NULL,
+  `pid` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for cnpse_contents
+-- ----------------------------
+CREATE TABLE `cnpse_contents` (
+  `id` int(11) NOT NULL,
+  `title` varchar(50) NOT NULL,
+  `cid` int(11) NOT NULL,
+  `purl` varchar(255) DEFAULT NULL,
+  `content` text,
+  `createtime` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for cnpse_contenttop
+-- ----------------------------
+CREATE TABLE `cnpse_contenttop` (
+  `id` int(11) NOT NULL,
+  `cid` int(11) NOT NULL,
+  `top1` int(11) DEFAULT NULL,
+  `top2` int(11) DEFAULT NULL,
+  `top3` int(11) DEFAULT NULL,
+  `top4` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
 -- ----------------------------
 -- Table structure for cnpse_users
 -- ----------------------------
@@ -23,3 +59,15 @@ CREATE TABLE `cnpse_users` (
 -- ----------------------------
 -- Records 
 -- ----------------------------
+INSERT INTO `cnpse_column` VALUES ('1', '机构介绍', null);
+INSERT INTO `cnpse_column` VALUES ('2', '通知公告', null);
+INSERT INTO `cnpse_column` VALUES ('3', '资讯聚焦', null);
+INSERT INTO `cnpse_column` VALUES ('4', '政策法规', null);
+INSERT INTO `cnpse_column` VALUES ('5', '机构公告', '2');
+INSERT INTO `cnpse_column` VALUES ('6', '培训招生', '2');
+INSERT INTO `cnpse_column` VALUES ('7', '中心要闻', '3');
+INSERT INTO `cnpse_column` VALUES ('8', '最新资讯', '3');
+INSERT INTO `cnpse_column` VALUES ('9', '视频中心', '3');
+INSERT INTO `cnpse_column` VALUES ('10', '国家政策', '4');
+INSERT INTO `cnpse_column` VALUES ('11', '部委法规', '4');
+INSERT INTO `cnpse_column` VALUES ('12', '中心规章', '4');
