42d9435d18127d739dbd9ab0283fe623762b6728
Jahs <jxtory@sina.com>
2018-10-13 22:13:37 +0800

    201810132213
    添加新内容，推送栏目列表

--- Engine/application/manager/controller/Content.php
+++ Engine/application/manager/controller/Content.php
@@ -7,6 +7,12 @@ class Content extends ManagerBase
 {
     public function index()
     {
+    	// 添加内容页面
+    	// 获取栏目列表
+    	$collist = db("column")->field("columnname")->where("pid is not null")->select();
+    	// 推送栏目列表
+    	$this->assign("collist", $collist);
+
     	return $this->fetch("index");
     }
 
